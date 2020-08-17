4d2dd7d15d772edd2bc196842e9a1c7315e81d03
junko911 <junkotahara911@gmail.com>
2020-08-17 12:08:19 -0400

    Add likes field

--- db/migrate/20200817152611_create_posts.rb
+++ db/migrate/20200817152611_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.string :description
+      t.integer :likes
 
       t.timestamps
     end
