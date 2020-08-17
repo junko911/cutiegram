e7298c4279f2fbe92a754c4e68650420d56353b0
junko911 <junkotahara911@gmail.com>
2020-08-17 16:16:34 -0400

    Merge master branch and fix errors

--- db/migrate/20200817152611_create_posts.rb
+++ db/migrate/20200817152611_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.string :description
-      t.integer :likes
+      t.integer :likes, :default => 0
 
       t.timestamps
     end
