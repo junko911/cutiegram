4c028ef9eb72535142ee394c9cd4cb69b0e1a7de
junko911 <junkotahara911@gmail.com>
2020-08-17 12:06:09 -0400

    Add like button

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :posts, except: [:index]
+  
+  resources :posts do
+    member do
+       patch :like
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
