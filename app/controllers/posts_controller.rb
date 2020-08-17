84a1a7430bf954226faeb959fb39313b191e5069
junko911 <junkotahara911@gmail.com>
2020-08-17 11:40:15 -0400

    Set up actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,42 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @post = Post.new
+  end
   
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user = @post.user
+    @post.destroy
+    redirect_to user_path(user) #redirect to user show page
+  end
+
+  private
+  
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description, :user_id)
+  end
+
 end
