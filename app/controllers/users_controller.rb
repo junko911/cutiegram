6c00b89ebc7fc911f3d43b97a28479080de7fab0
irosen419 <irosen419@gmail.com>
2020-08-17 12:40:13 -0400

    added errors form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
         if user.valid?
             redirect_to user_path(@user)
         else
-            flash[:errors] = @user.errors.full_messages
             redirect_to new_user_path
         end
     end
