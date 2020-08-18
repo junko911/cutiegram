556efc3b951a54076f10b592b55290a433f945da
irosen419 <irosen419@gmail.com>
2020-08-18 19:54:05 -0400

    finished post show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :post_tags
   resources :tags
-  resources :posts, except: [:index]
+  resources :posts
   
   resources :posts do
     resources :comments
