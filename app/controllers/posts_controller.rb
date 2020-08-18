f54eae4286385ef5134a182553738ae9c86da835
irosen419 <irosen419@gmail.com>
2020-08-17 23:19:46 -0400

    updated layouts for navbar and users show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    if @post.save
+    if @post.save!
       redirect_to @post
     else
       render :new
