820e1024cb85554b9771d9d5b20cb9e340d446e6
junko911 <junkotahara911@gmail.com>
2021-03-05 09:30:34 -0500

    Add dependent to likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
   has_many :posts, :dependent => :destroy
   has_many :comments, :dependent => :destroy
-  has_many :likes
+  has_many :likes, :dependent => :destroy
   
   has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   has_many :followeds, through: :active_relationships, source: :followed
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
   
-
   validates :username, presence: { message: 'You need a username! How else will we know what to call you?'}
   validates :username, uniqueness: { message: 'Shoot! Someone is already using this username...'}
   has_attached_file :image,
@@ -19,7 +18,7 @@ class User < ApplicationRecord
                         }
 
   validates_attachment :image,
-                     content_type: { content_type: /\Aimage\/.*\z/ }
+                    content_type: { content_type: /\Aimage\/.*\z/ }
 
   has_secure_password
 
